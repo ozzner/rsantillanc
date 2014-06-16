8db3f81cf97d234d3b2e3f13a3ea615bbd1c1a62
rsantillanc <rsantillanc@57230d69-6c2c-9573-b837-351b7d564c6b>
2014-06-16 19:01:05 +0000

    ok

--- api/db/db_server.php
+++ api/db/db_server.php
@@ -3,7 +3,7 @@
 /**
  * Configuracion del servidor de base de datos
  */
-    const DB_HOST = "127.0.0.1";
+    const DB_HOST = "localhost";
     const DB_USER = "root";
     const DB_PASS = "";
     const DB_NAME = "db_apprade";
--- api/v1/index.php
+++ api/v1/index.php
@@ -1,19 +1,4 @@
 <?php
-echo 'hello ozzner';
-
-$mail = $_POST['correo'];
-$sex = $_POST['sexo'];
-$nom = $_POST['nombre'];
-$feh = $_POST['fecha'];
-$app = $_POST['apellido1'];
-$apm = $_POST['apellido2'];
 
 
-
-include_once '../dao../dao_usuario.php';
-
-$usuario = new usuario();
-$respuesta = $usuario->registrar($mail, $sex, $nom, $feh, $app, $apm);
-
-echo 'Esta es la respuesta: '.$respuesta;
 ?>

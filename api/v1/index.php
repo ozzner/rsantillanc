0ec25e8802e00747f1c3be4771c2d602116c2e4d
rsantillanc <rsantillanc@57230d69-6c2c-9573-b837-351b7d564c6b>
2014-06-16 19:16:11 +0000

    ok

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,4 +1,34 @@
 <?php
 
 
+#Includes
+include_once '/';
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+switch ($method) {
+  case 'PUT':
+    echo($method);
+    break;
+  case 'POST':
+       echo($method);
+    break;
+  case 'GET':
+      echo($method);
+    break;
+  case 'HEAD':
+ echo($method);
+    break;
+  case 'DELETE':
+     echo($method);
+    break;
+  case 'OPTIONS':
+   echo($method);
+    break;
+  default:
+ echo($method);
+    break;
+}
+
 ?>

87ae11743b82738172e4e09e78311578937ecc77
rsantillanc <rsantillanc@57230d69-6c2c-9573-b837-351b7d564c6b>
2014-06-18 21:38:23 +0000

    ok

--- api/db/db_server.php
+++ api/db/db_server.php
@@ -4,7 +4,7 @@
  * Configuracion del servidor de base de datos
  */
     const DB_HOST = "localhost";
-    const DB_USER = "ro ot";
+    const DB_USER = "root";
     const DB_PASS = "";
     const DB_NAME = "db_apprade";
 
